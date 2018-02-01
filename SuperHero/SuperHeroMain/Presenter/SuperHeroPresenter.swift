56ff8916ef4332356b9f4e5eb83e7e048f0c831b
Raúl Pedraza <raul@corpora360.com>
2018-02-02 00:19:26 +0100

    Setup presenter for setup table view in view.

--- SuperHero/SuperHeroMain/Presenter/SuperHeroPresenter.swift
+++ SuperHero/SuperHeroMain/Presenter/SuperHeroPresenter.swift
@@ -1,5 +1,21 @@
-import Foundation
+import UIKit
 
-class SuperHeroPresenter {
+class SuperHeroPresenter: SuperHeroPresenterProtocol {
+    var interactor: SuperHeroInteractorProtocol!
+    var routing: SuperHeroRoutingProtocol!
+    weak var view: SuperHeroViewProtocol!
     
+    //MARK: SuperHeroPresenterProtocol
+    
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRows(at section: Int) -> Int {
+        return 1
+    }
+    
+    func buildCell(at indexpath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
--- SuperHero/SuperHeroMain/Presenter/SuperHeroPresenterProtocol.swift
+++ SuperHero/SuperHeroMain/Presenter/SuperHeroPresenterProtocol.swift
@@ -1,5 +1,7 @@
-import Foundation
+import UIKit
 
 protocol SuperHeroPresenterProtocol {
-    
+    var numberOfSections: Int { get }
+    func numberOfRows(at section: Int) -> Int
+    func buildCell(at indexpath: IndexPath) -> UITableViewCell
 }
