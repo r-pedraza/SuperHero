4bd735f6ff23a0c9490561a07c6a55815f53ae6e
Raúl Pedraza <raul@corpora360.com>
2018-02-02 00:19:48 +0100

    Routing setup.

--- SuperHero/SuperHeroMain/Routing/SuperHeroRouting.swift
+++ SuperHero/SuperHeroMain/Routing/SuperHeroRouting.swift
@@ -1,5 +1,21 @@
-import Foundation
+import UIKit
 
-class SuperHeroRouting {
+class SuperHeroRouting: SuperHeroRoutingProtocol {
+    let navigationController: UINavigationController!
+    let viewController: SuperHeroViewController!
     
+    init(navigationController: UINavigationController, viewController: SuperHeroViewController) {
+        self.navigationController = navigationController
+        self.viewController = viewController
+    }
+    
+    func setup() {
+        let interactor = SuperHeroInteractor()
+        let presenter = SuperHeroPresenter()
+        interactor.presenter = presenter
+        presenter.interactor = interactor
+        presenter.routing = self
+        presenter.view = viewController
+        viewController.presenter = presenter
+    }
 }
