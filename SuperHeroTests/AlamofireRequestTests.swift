6471b7bdd080c91dad1aef4f47ae68dc86b75cbd
Raúl Pedraza <raul@corpora360.com>
2018-02-01 22:53:48 +0100

    Test in marvel API to tests SuperHeroes model and SuperHero model.

--- SuperHeroTests/AlamofireRequestTests.swift
+++ SuperHeroTests/AlamofireRequestTests.swift
@@ -14,13 +14,42 @@ class AlamofireRequestTests: XCTestCase {
         Alamofire.request(request)
             .validate(statusCode: 200..<300)
             .responseJSON(completionHandler: { (response) in
-                if let JSON = response.result.value as? [String: AnyObject], let superHeroesDic = JSON["superheroes"] as? [[String: AnyObject]]  {
-                    debugPrint(superHeroesDic)
-                    XCTAssertNotNil(JSON)
-                    XCTAssertNotNil(superHeroesDic)
+                switch response.result {
+                case .success:
+                    if let JSON = response.result.value as? [String: AnyObject], let superHeroesDic = JSON["superheroes"] as? [[String: AnyObject]]  {
+                        debugPrint(superHeroesDic)
+                        XCTAssertNotNil(JSON)
+                        XCTAssertNotNil(superHeroesDic)
+                    }
+                case .failure(let error):
+                    debugPrint(error.localizedDescription)
                 }
-            expect.fulfill()
-        })
+                expect.fulfill()
+            })
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testParseMarvelModelWithApiResponse() {
+        let request = Router.getMarvelSuperHeroes()
+        let expect = expectation(description: "Get some marvels super heroes!")
+        Alamofire.request(request)
+            .validate(statusCode: 200..<300)
+            .responseData(completionHandler: { (response) in
+                    switch response.result {
+                    case .success(let data):
+                        let decoder = JSONDecoder()
+                        do {
+                            let response = try decoder.decode(SuperHeroes.self, from: data)
+                            XCTAssertFalse(response.superheroes.isEmpty)
+                        } catch let error as NSError {
+                            debugPrint(error.localizedDescription)
+                        }
+                        
+                    case .failure(let error):
+                        debugPrint(error.localizedDescription)
+                    }
+                expect.fulfill()
+                })
         waitForExpectations(timeout: 10, handler: nil)
     }
 }
