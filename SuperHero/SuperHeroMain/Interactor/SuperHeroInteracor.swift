a5cce8b7fee68f633ba837b4dd0e3566de5f4ea3
Raúl Pedraza <raul@corpora360.com>
2018-02-02 00:19:06 +0100

    VIPER connected elements.

--- SuperHero/SuperHeroMain/Interactor/SuperHeroInteracor.swift
+++ SuperHero/SuperHeroMain/Interactor/SuperHeroInteracor.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class SuperHeroInteractor {
-    
+class SuperHeroInteractor: SuperHeroInteractorProtocol {
+    var presenter: SuperHeroPresenterProtocol!
 }
--- SuperHero/SuperHeroMain/View/SuperHeroViewProtocol.swift
+++ SuperHero/SuperHeroMain/View/SuperHeroViewProtocol.swift
@@ -1,9 +1,5 @@
-//
-//  SuperHeroViewProtocol.swift
-//  SuperHero
-//
-//  Created by Raul Pedraza on 01/02/2018.
-//  Copyright © 2018 Raul Pedraza. All rights reserved.
-//
-
 import Foundation
+
+protocol SuperHeroViewProtocol: class {
+    
+}
