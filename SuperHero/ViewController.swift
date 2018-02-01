33451d3a8a6828aa699909bf0a186a557f5d8014
Raúl Pedraza <raul@corpora360.com>
2018-02-01 09:30:09 +0100

    Initial Commit

---
+++ SuperHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperHero
+//
+//  Created by Raul Pedraza on 01/02/2018.
+//  Copyright © 2018 Raul Pedraza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
