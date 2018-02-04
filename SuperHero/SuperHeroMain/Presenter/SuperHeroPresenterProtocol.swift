63c2e2198ef816ab2211ffdff37ec2bfad738aa9
Raúl Pedraza <raul@corpora360.com>
2018-02-04 08:08:18 +0100

    Add Methods in Presenter protocol to get super hero at index and reload table view.

--- SuperHero/SuperHeroMain/Presenter/SuperHeroPresenterProtocol.swift
+++ SuperHero/SuperHeroMain/Presenter/SuperHeroPresenterProtocol.swift
@@ -4,4 +4,6 @@ protocol SuperHeroPresenterProtocol {
     var numberOfSections: Int { get }
     func numberOfRows(at section: Int) -> Int
     func buildCell(at indexpath: IndexPath) -> UITableViewCell
+    func superHero(at: IndexPath) -> ViewModel
+    func reloadData()
 }
